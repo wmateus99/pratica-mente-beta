ea8a9c555c65ae5ee7476427d2e23cb69df78d15
wmateus99 <wandersonmateus07@gmail.com>
2025-08-09 03:33:17 -0300

    add

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -3,6 +3,6 @@ export function criarMenu() {
     navMenu.innerHTML = `<a href="/index.html">Início</a>
                         <a href="/src/pages/auxiliares.html">Auxiliares</a>
                         <a href="/src/pages/revisao.html">Revisão</a>
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeQ3yRtgFmXARiRfHZWuOdGw8eTfG9rKPg6Gu_sxx_ga-Iw7Q/viewform">Prova</a>
+                        <a href="/src/pages/provas/informatica-essencial.html">Prova</a>
                         <a href="/src/pages/textos.html">Textos</a>`
 }
