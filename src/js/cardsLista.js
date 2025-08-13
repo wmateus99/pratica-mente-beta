ec8153955460b1d586fa1758faf2341035a4967a
wmateus99 <wandersonmateus07@gmail.com>
2025-08-13 00:57:36 -0300

    add

--- src/js/cardsLista.js
+++ src/js/cardsLista.js
@@ -244,7 +244,7 @@ export const cardsProntos = [
             titulo: 'Atividade 02',
             descricao: 'Criar apresentação no PowerPoint 2019 com mídia, animações e recursos visuais.',
             imagem: '/public/img/cards/powerpoint.png',
-            link: './powerpoint/03.html'
+            link: './powerpoint/02.html'
         })
     },
 
