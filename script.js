273f0ce215c482dbb3ff8cc417cd63158dabf14d
wmateus99 <wandersonmateus07@gmail.com>
2025-08-14 12:23:44 -0300

    add

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 import { criarFooter } from "./src/js/footer.js"
 import { criarMenu } from "./src/js/nav-menu.js"
 import { cardsProntos } from './src/js/cardsLista.js';
-import './src/js/loader.js'
+
+const favicon = document.createElement('link');
+favicon.rel = "shortcut icon";
+favicon.href = "/public/img/logo_icone_praticamente_white.svg";
+favicon.type = "image/x-icon"
+
 
 
 window.addEventListener("DOMContentLoaded", () => {
+    document.head.appendChild(favicon)
     document.title = "PraticaMente"
     criarMenu()
     criarFooter()
