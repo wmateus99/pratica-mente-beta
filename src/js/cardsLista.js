13500a6eb63f8340e4110aced08e9cbe5da006a5
wmateus99 <wandersonmateus07@gmail.com>
2025-09-02 20:36:18 -0300

    add

--- src/js/cardsLista.js
+++ src/js/cardsLista.js
@@ -287,6 +287,16 @@ export const cardsProntos = [
             link: './powerpoint/06.html'
         })
     },
+    {
+        container: '.revisao-powerpoint',
+        card: criarCard({
+            classAdd: "bg-powerpoint",
+            titulo: 'Atividade 07',
+            descricao: 'Criar apresentação no PowerPoint 2019 com mídia, animações e recursos visuais.',
+            imagem: '/public/img/cards/powerpoint.png',
+            link: './powerpoint/07.pdf'
+        })
+    },
 
 
     // Atividades de Windows
