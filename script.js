16de70704c2e7252ba691fba2f40a43cd235d517
wmateus99 <wandersonmateus07@gmail.com>
2025-10-09 13:00:24 -0300

    Adição do Sistema "Ensino Web"

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 import { getFileName } from "./src/js/get-file-name.js";
 import { setFavicon } from "./src/js/set-favicon.js";
 import { navMenu } from "./src/js/nav-menu.js";
-import { banner } from "./src/js/banner.js";
 // import { cardAnimation } from "./src/js/card-animation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    banner();
 
     const logo = document.querySelector('.logo');
 
---
+++ src/pages/ensino-web/aulas/css-aulas/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    const owl = $('.owl-carousel');
+    let savedIndex = sessionStorage.getItem('currentSlide') || 0;
+
+    owl.owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        autoplay: false,
+        mouseDrag: false,
+        touchDrag: false,
+        startPosition: savedIndex // inicia no slide salvo
+    });
+
+    $(document).on('click', '.proximo', function() {
+        owl.trigger('next.owl.carousel');
+    });
+
+    $(document).on('click', '.anterior', function() {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    owl.on('changed.owl.carousel', function(event) {
+        const currentIndex = event.item.index;
+        sessionStorage.setItem('currentSlide', currentIndex); // salva índice na sessão
+    });
+});
+
